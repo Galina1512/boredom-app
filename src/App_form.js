58dbc58cb767eadd373dbd6de6da94c67e36ae2f
Galina1512 <Kostina13@gmail.com>
2022-03-09 10:08:21 +0300

    change color

--- src/App_form.js
+++ src/App_form.js
@@ -12,19 +12,22 @@ function App(){
     const changetext = async ()=>{
     const responce = await fetch('http://www.boredapi.com/api/activity/');
     const data = await responce.json();
-    setState(data.activity);
-    setIsGreen(!isGreen) 
-
+    setState(data.activity)
 }
-    useEffect(()=>{
-        changetext();
-    }, [])
+useEffect(()=>{
+    changetext()
+}, [])
+
+
 
 return(
     <div className="App">
         <h1 className="header">If you're bored, click on the apple and see what you can do</h1>
-       <button className="btn" onClick={changetext}><img src={image} width="60px"  alt="apple" /></button>
-       <h2 className="advice" style={{ color: isGreen ? "green" : "red" }} >{state}{isGreen} </h2>
+    <form 
+       > 
+       <button className="btn"><img src={image} width="60px" onClick={() => setIsGreen(!isGreen) } alt="apple" /></button>
+       <h2 className="advice" style={{ color: isGreen ? "green" : "red" }} >{state} </h2>
+    </form>
     <div className="container">
             <video autoPlay muted loop className="video">
             <source src={video} type='video/mp4'/>
